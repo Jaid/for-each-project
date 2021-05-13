11b4486c8433b09bc2b72bfc7163a25fbd5d06cf
Jaid <jaid.jsx@gmail.com>
2021-05-13 23:32:08 +0200

    Improved logging
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/runForProjects.js
+++ src/lib/runForProjects.js
@@ -1,5 +1,6 @@
 import chalk from "chalk"
 import globby from "globby"
+import hasContent from "has-content"
 import readableMs from "readable-ms"
 import simpleGit from "simple-git/promise"
 
@@ -14,7 +15,7 @@ const indentLog = message => {
 }
 
 /**
- * @return {Promise<Project[]>}
+ * @return {Promise<void>}
  */
 export default async job => {
   const dirs = await globby("*", {
@@ -22,33 +23,59 @@ export default async job => {
     cwd: "P:/Git",
     onlyDirectories: true,
   })
+  let pullCount = 0
+  let okCount = 0
+  const errorNames = []
+  const dirtyNames = []
   for (const dir of dirs) {
     const time = Date.now()
     const project = new Project(dir)
     await project.init()
-    log(chalk.magenta(`╭ ${project.folderName} `.padEnd(60, "╴")))
-    if (project.folderName === "for-each-project") {
-      log(chalk.gray("This is me!"))
-      continue
+    try {
+      log(chalk.magenta(`╭ ${project.folderName} `.padEnd(60, "╴")))
+      if (project.folderName === "for-each-project") {
+        log(chalk.gray("This is me!"))
+        continue
+      }
+      const gitRepository = simpleGit(dir)
+      if (!project.pkg) {
+        log(chalk.gray(`${project.folderName} has no pkg`))
+        continue
+      }
+      const isGit = await gitRepository.checkIsRepo()
+      if (!isGit) {
+        log(chalk.gray(`${project.folderName} is not git`))
+        continue
+      }
+      const status = await gitRepository.status()
+      if (!status.isClean()) {
+        log(chalk.gray(`${project.folderName} is dirty`))
+        dirtyNames.push(project.folderName)
+        continue
+      }
+      const pulled = await project.pull()
+      if (pulled) {
+        pullCount++
+      }
+      await job(project, indentLog)
+      await project.gitFlush("Modified in bulk with Jaid/for-each-project")
+      log(`Processed ${project.folderName} in ${readableMs(Date.now() - time)}`)
+      okCount++
+    } catch (error) {
+      log(error)
+      errorNames.push(project.folderName)
     }
-    const gitRepository = simpleGit(dir)
-    if (!project.pkg) {
-      log(chalk.gray(`${project.folderName} has no pkg`))
-      continue
-    }
-    const isGit = await gitRepository.checkIsRepo()
-    if (!isGit) {
-      log(chalk.gray(`${project.folderName} is not git`))
-      continue
-    }
-    const status = await gitRepository.status()
-    if (!status.isClean()) {
-      log(chalk.gray(`${project.folderName} is dirty`))
-      continue
-    }
-    await project.pull()
-    await job(project, indentLog)
-    await project.gitFlush("Modified in bulk with jaid/for-each-project")
-    log(`Processed ${project.folderName} in ${readableMs(Date.now() - time)}`)
+  }
+  if (hasContent(errorNames)) {
+    log(`${errorNames.length} errored: ${errorNames.join(", ")}`)
+  }
+  if (hasContent(dirtyNames)) {
+    log(`${dirtyNames.length} dirty: ${dirtyNames.join(", ")}`)
+  }
+  if (pullCount) {
+    log(`${pullCount} pulled`)
+  }
+  if (okCount) {
+    log(`${okCount} ok`)
   }
 }
